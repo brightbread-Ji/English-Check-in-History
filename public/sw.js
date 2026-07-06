e6aa24e23c4d7ce4ffed145086ffa423318f9b64
brightbread-Ji <275745878+brightbread-Ji@users.noreply.github.com>
2026-07-07 00:37:40 +0800

    refresh app shell from network

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'english-learning-tracker-v1'
+const CACHE_NAME = 'english-learning-tracker-v2'
 const APP_ASSETS = ['/', '/manifest.webmanifest', '/favicon.svg']
 
 self.addEventListener('install', (event) => {
@@ -17,6 +17,19 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') {
+    return
+  }
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone()
+          event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.put('/', copy)))
+          return response
+        })
+        .catch(() => caches.match('/')),
+    )
     return
   }
 
